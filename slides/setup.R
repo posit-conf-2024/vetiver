cf00c8a8c22d0f000d20173249cc189285236d9c
isabelizimm <isabel.zimmerman@rstudio.com>
2024-05-07 15:31:45 -0400

    init

---
+++ slides/setup.R
@@ -0,0 +1,10 @@
+knitr::opts_chunk$set(
+  digits = 3,
+  comment = "#>"
+)
+
+# devtools::install_github("gadenbuie/countdown")
+library(countdown)
+library(ggplot2)
+theme_set(theme_minimal(base_size = 16))
+options(cli.width = 70)
